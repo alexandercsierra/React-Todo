efe0aadd6fbfed5629a8fe7df41cec132acd0554
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-01-01 12:49:01 -0500

    mapped todos displaying

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,11 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react'
+
+const TodoList = props => {
+return <div>
+    {props.todos.map(todo => {
+        return <h3>{todo.task}</h3>
+    })}
+</div>
+}
+
+export default TodoList
